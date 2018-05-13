1367a56763a5aac6bd885fbc4ff07cddecac14ca
stone li <inkstone2006@163.com>
2018-05-13 16:41:30 +0800

    test
    test

---
+++ import_data.sql
@@ -0,0 +1,10 @@
+insert into student(id,name,sex,age,course_id) values(1,'杨超','男',30,1);
+insert into student(id,name,sex,age,course_id) values(2,'邓香玉','女',35,1);
+insert into student(id,name,sex,age,course_id) values(3,'曾伟','男',23,2);
+insert into student(id,name,sex,age,course_id) values(4,'润淑君','女',26,2);
+insert into student(id,name,sex,age,course_id) values(5,'李娇','男',29,3);
+insert into student(id,name,sex,age,course_id) values(6,'王丽坤','女',21,3);
+
+insert into course(id,name) values(1,'语文');
+insert into course(id,name) values(2,'数学');
+insert into course(id,name) values(3,'英语');
